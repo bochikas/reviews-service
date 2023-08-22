17ce3839a52ee71dd348265fadc8c1e20cb918fa
Damir Kambarov <damir.kambarov@qualixdev.com>
2023-08-22 19:36:37 +0500

    users app migrates

--- src/users/models.py
+++ src/users/models.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 
 
 class GenderType(models.IntegerChoices):
@@ -12,26 +11,13 @@ class GenderType(models.IntegerChoices):
 class User(AbstractUser):
     """Пользователь."""
 
-    username = models.CharField(_("username"), max_length=150, unique=True,
-                                help_text=_(
-                                    "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
-                                ),
-                                validators=[AbstractUser.username_validator],
-                                error_messages={
-                                    "unique": _("A user with that username already exists."),
-                                },
-                                null=True, blank=True)
-    email = models.EmailField(_("email address"), unique=True)
     is_company = models.BooleanField(default=False)
     is_verified = models.BooleanField(default=False)
     register_date = models.DateField(verbose_name='Дата регистрации', auto_now_add=True)
-    last_activity = models.DateTimeField(verbose_name='Последняя активность')
+    last_activity = models.DateTimeField(verbose_name='Последняя активность', null=True)
     gender = models.CharField(max_length=6, choices=GenderType.choices, verbose_name='Пол')
     photo = models.ImageField(blank=True, null=True, verbose_name='Аватар')
     dob = models.DateField(blank=True, null=True, verbose_name='Дата рождения')
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
 
 
 class Account(models.Model):
